eeb1411e283430587b347f52c70d3347b5e4dc59
rpovolotsky <qwertyp26@gmail.com>
2016-02-25 10:42:48 +0300

    init

---
+++ Test/MathExt.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    static class MathExt
+    {
+        /// <summary>
+        /// Проверка на треугольник
+        /// </summary>
+        /// <param name="a">Сторона A</param>
+        /// <param name="b">Сторона B</param>
+        /// <param name="c">Сторона C</param>
+        /// <returns></returns>
+        public static bool IsTringle(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b&&c + b > a) return true;
+            return false;
+        }
+
+
+        /// <summary>
+        /// Проверка на треугольник
+        /// </summary>
+        /// <param name="a">Сторона A</param>
+        /// <param name="b">Сторона B</param>
+        /// <param name="c">Сторона C</param>
+        /// <returns></returns>
+
+        public static bool IsStraightTringle(double a, double b, double c)
+        {
+            if (a>b&&a>c)
+            {
+                if (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }else if (b>a&&b>c)
+            {
+                if (Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }else if(c>a&&c>b)
+            {
+                if (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Вычисление плошади прямоугольного  труегольника треугольника
+        /// </summary>
+        /// <param name="a">Сторона A</param>
+        /// <param name="b">Сторона B</param>
+        /// <param name="c">Сторона C</param>
+        /// <returns>Площадь</returns>
+
+        public static double TringleArea(double a, double b, double c)
+        {
+            double rez = 0;
+            if (a > b && a > c)
+            {
+                rez = (b * c) / 2;
+            }
+            else if (b > a && b > c)
+            {
+                rez = (a * c) / 2;
+            }
+            else if (c > a && c > b)
+            {
+                rez = (a * b ) / 2;
+            }
+            return rez;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ok = false;
+            double a=0, b=0, c=0,rez;
+            while (!ok)
+            {
+                Console.WriteLine("Введите стороно A");
+               ok= Double.TryParse(Console.ReadLine().Replace(".",","), out a);
+               if (!ok) Console.WriteLine("Это не число!");
+
+            }
+            ok = false;
+            while (!ok)
+            {
+                Console.WriteLine("Введите стороно B");
+                ok = Double.TryParse(Console.ReadLine().Replace(".", ","), out b);
+                if(!ok)Console.WriteLine("Это не число!");
+            }
+            ok = false;
+            while (!ok)
+            {
+                Console.WriteLine("Введите стороно C");
+                ok = Double.TryParse(Console.ReadLine().Replace(".", ","), out c);
+                if (!ok) Console.WriteLine("Это не число!");
+                
+            }
+            if (MathExt.IsTringle(a, b, c))
+            {
+                if (MathExt.IsStraightTringle(a, b, c))
+                {
+                    rez = MathExt.TringleArea(a, b, c);
+                }
+                else
+                {
+                    Console.WriteLine("Это не прямоугольный треугольник!");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Это не треугольник!");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine(string.Format("Площадь треугольника = {0}.",rez));
+            Console.ReadKey();
+
+
+        }
+    }
+}
